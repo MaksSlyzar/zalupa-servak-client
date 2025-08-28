d09068bfa1d50012d7f49460f3aeed7355a0ab64
MaksSlyzar <maks.slyzar@gmail.com>
2025-08-28 08:33:08 +0300

    create downloader

---
+++ downloader/takelist.py
@@ -0,0 +1,25 @@
+import os
+import platform
+
+system = platform.system()
+if system == "Linux":
+    MINECRAFT_DIR = os.path.expanduser("~/.minecraft")
+elif system == "Windows":
+    MINECRAFT_DIR = os.path.join(os.getenv("APPDATA"), ".minecraft")
+else:
+    MINECRAFT_DIR = os.getcwd()
+
+MODS_DIR = os.path.join(MINECRAFT_DIR, "mods")
+if not os.path.exists(MODS_DIR):
+    os.makedirs(MODS_DIR)
+
+mods_list_path = os.path.join("./mods_list.txt")
+
+with open(mods_list_path, "w") as f:
+    for root, dirs, files in os.walk(MODS_DIR):
+        for file in files:
+            if file.endswith(".jar"):
+                f.write(file + "\n")
+
+print(f"Mods list saved to: {mods_list_path}")
+
